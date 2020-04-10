130b3976f4033e19b41f69ebfe726bbd18ccdd19
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-04-10 15:53:51 +0200

    Setting up action button

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -12,15 +12,36 @@ class MainTabController: UITabBarController {
 
     // MARK: - Properties
     
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named:"new_tweet"), for: .normal)
+        return button
+    }()
+    
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureViewControllers()
+        configureUI()
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.addSubview(actionButton)
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                            right: view.safeAreaLayoutGuide.rightAnchor,
+                            paddingBottom: 64,
+                            paddingRight: 16,
+                            width: 56,
+                            height: 56)
+        actionButton.layer.cornerRadius = 56 / 2
+    }
     
     func configureViewControllers() {
         
