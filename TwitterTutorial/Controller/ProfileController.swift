8e9c3f10f9846cf4c7e2f00cd2c46e84ad90058f
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-04-13 16:36:50 +0200

    Adding Profile Header UI

--- TwitterTutorial/Controller/ProfileController.swift
+++ TwitterTutorial/Controller/ProfileController.swift
@@ -24,6 +24,7 @@ class ProfileController: UICollectionViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        navigationController?.navigationBar.barStyle = .black
         navigationController?.navigationBar.isHidden = true
     }
     
