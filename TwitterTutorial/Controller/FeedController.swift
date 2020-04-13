d6b6ac237819309c4b620b1a9af70be44579451d
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-04-13 14:23:49 +0200

    Populating Feed with tweets

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -21,6 +21,10 @@ class FeedController: UICollectionViewController {
         }
     }
     
+    private var tweets = [Tweet]() {
+        didSet { collectionView.reloadData() }
+    }
+    
     
     // MARK: - Lifecycle
     
@@ -34,7 +38,7 @@ class FeedController: UICollectionViewController {
     
     func fetchTweets() {
         TweetService.shared.fetchTweets { tweets in
-            print("Tweets are \(tweets)")
+            self.tweets = tweets
         }
     }
     
@@ -65,9 +69,12 @@ class FeedController: UICollectionViewController {
     }
 }
 
+
+// MARK: - UICollectionViewDelegate/DataSource
+
 extension FeedController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return tweets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
