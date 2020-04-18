c0aefc2f695bd98597fa334372b10d83e8ce6c7b
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-04-18 22:27:15 +0200

    ActionSheet Cell and cancel button

--- TwitterTutorial/Utils/ActionSheetLauncher.swift
+++ TwitterTutorial/Utils/ActionSheetLauncher.swift
@@ -28,6 +28,26 @@ class ActionSheetLauncher: NSObject {
         return view
     }()
     
+    private lazy var footerView: UIView = {
+        let view = UIView()
+        view.addSubview(cancelButton)
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.anchor(left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 12, paddingRight: 12)
+        cancelButton.centerY(inView: view)
+        cancelButton.layer.cornerRadius = 50 / 2
+        return view
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     init(user: User) {
@@ -56,16 +76,17 @@ class ActionSheetLauncher: NSObject {
         blackView.frame = window.frame
         
         window.addSubview(tableView)
-        tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: 300)
+        let height = CGFloat(3 * 60) + 100
+        tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
         
         UIView.animate(withDuration: 0.5) {
             self.blackView.alpha = 1
-            self.tableView.frame.origin.y -= 300
+            self.tableView.frame.origin.y -= height
         }
     }
     
     func configureTableView() {
-        tableView.backgroundColor = .red
+        tableView.backgroundColor = .white
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = 60
@@ -73,14 +94,20 @@ class ActionSheetLauncher: NSObject {
         tableView.layer.cornerRadius = 5
         tableView.isScrollEnabled = false
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.register(ActionSheetCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 }
 
 // MARK: - UITableViewDelegate
 
 extension ActionSheetLauncher: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footerView
+    }
     
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 60
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -91,7 +118,7 @@ extension ActionSheetLauncher: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ActionSheetCell
         return cell
     }
 }
---
+++ TwitterTutorial/View/ActionSheetCell.swift
@@ -0,0 +1,50 @@
+//
+//  ActionSheetCell.swift
+//  TwitterTutorial
+//
+//  Created by Oscar Rodriguez Garrucho on 18/04/2020.
+//  Copyright © 2020 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let optionImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "twitter_logo_blue")
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Test Option"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(optionImageView)
+        optionImageView.centerY(inView: self)
+        optionImageView.anchor(left: leftAnchor, paddingLeft: 8)
+        optionImageView.setDimensions(width: 36, height: 36)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: self)
+        titleLabel.anchor(left: optionImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+}
