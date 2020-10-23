182be635440a3353c9b4c102b6982650a405c3ca
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-10-23 14:56:34 +0200

    Notification data model

---
+++ TwitterTutorial/Model/Notification.swift
@@ -0,0 +1,41 @@
+//
+//  Notification.swift
+//  TwitterTutorial
+//
+//  Created by Oscar Rodriguez Garrucho on 23/10/20.
+//  Copyright © 2020 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationType: Int {
+    case follow
+    case like
+    case reply
+    case retweet
+    case mention
+}
+
+struct Notification {
+
+    let tweetID: String?
+    var timestamp: Date!
+    let user: User
+    var tweet: Tweet?
+    var type: NotificationType!
+
+    init(user: User, tweet: Tweet?, dictionary: [String: AnyObject]) {
+        self.user = user
+        self.tweet = tweet
+
+        self.tweetID = dictionary["tweetID"] as? String ?? ""
+
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+
+        if let type = dictionary["type"] as? Int {
+            self.type = NotificationType(rawValue: type)
+        }
+    }
+}
